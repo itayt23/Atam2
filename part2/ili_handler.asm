9c297baace606a53c108867c480629a09dbc1fea
Adam Katav <adamkatav@gmail.com>
2022-05-15 19:09:17 +0300

    Hara

--- part2/ili_handler.asm
+++ part2/ili_handler.asm
@@ -2,6 +2,45 @@
 
 .text
 .align 4, 0x90
+
+
+
 my_ili_handler:
-  ####### Some smart student's code here #######
+  xor %r9, %r9
+  xor %r10, %r10
+  xor %r11, %r11
+  xor %rdi, %rdi
+  movq (%rsp), %rdx
+  movb %rdx, %r9 # First byte
+  movb $8, %cl
+  shr %cl, %rdx
+  movb %rdx, %r10 # Second byte
+  shr %cl, %rdx
+  movb %rdx, %r11 # Third byte
+  cmp $0x0F, %dl
+  jne one_byte
+  cmp $0x0F3A, %dx
+  jne check_two_bytes
+  one_byte:
+  movq %r9, %rdi
+  call what_to_do
+  jmp end_brain_fuck
+  check_two_bytes:
+  cmp $0x0F38, %dx
+  jne two_bytes
+  jmp one_byte
+  two_bytes:
+  movq %r10, %rdi
+  call what_to_do
+  jmp end_brain_fuck
+  end_brain_fuck:
+  cmp $0, %rax
+  jne handle
+  jmp *old_ili_handler
+  end:
   iretq
+
+
+handle:
+  movq %rax, %rdi
+  jmp end
