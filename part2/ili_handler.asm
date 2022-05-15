5606a4df9257dbe92942ae828d892964efc685c2
Adam Katav <adamkatav@gmail.com>
2022-05-15 20:52:17 +0300

    asdasdasdasd

--- part2/ili_handler.asm
+++ part2/ili_handler.asm
@@ -9,32 +9,19 @@ handle:
 
 
 my_ili_handler:
-  jmp end
-  xor %r9, %r9
-  xor %r10, %r10
-  xor %r11, %r11
+  xor %rdx, %rdx
+  xor %rax, %rax
   xor %rdi, %rdi
   movq (%rsp), %rdx
-  movq %rdx, %r9 # First byte
-  movb $8, %cl
-  shr %cl, %rdx
-  movq %rdx, %r10 # Second byte
-  shr %cl, %rdx
-  movq %rdx, %r11 # Third byte
   cmp $0x0F, %dl
   jne one_byte
-  cmp $0x0F3A, %dx
-  jne check_two_bytes
-  one_byte:
-  movq %r9, %rdi
+  movb %dh, %al
+  movq %rax, %rdi
   call what_to_do
   jmp end_brain_fuck
-  check_two_bytes:
-  cmp $0x0F38, %dx
-  jne two_bytes
-  jmp one_byte
-  two_bytes:
-  movq %r10, %rdi
+  one_byte:
+  movb %dl, %al
+  movq %rax, %rdi
   call what_to_do
   jmp end_brain_fuck
   end_brain_fuck:
